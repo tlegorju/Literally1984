32f492dc1ec6407525a781cf3f0b6146b47af63b
tlegorju <daphnerenauld@gmail.com>
2025-08-24 18:34:47 +0200

    Pushed LD

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -6,6 +6,13 @@ enum WordType {OLD_WORD, NEW_WORD, NEUTRAL_WORD}
 var words = []
 var curStep : int = 2
 var maxStep : int = 3
+var player: CharacterBody3D;
+
+var next_dialogue;
+var next_dialogue_name;
+var next_pos;
+var next_max_dist;
+var next_signal;
 
 func _ready() -> void:
 	words.clear()
@@ -39,6 +46,8 @@ func _ready() -> void:
 	"newWord": "individual" })
 	words.append(stepArray3)
 	
+	player = get_tree().get_first_node_in_group("Player");
+	
 func getCurWords():
 	if curStep>=0 && curStep < maxStep:
 		return words[curStep]
@@ -49,3 +58,21 @@ func nextStep():
 	
 func isFinished():
 	return curStep == maxStep
+	
+
+func try_show_dialog(next_dia, next_dia_name, next_p, next_max_d, next_sign):
+	next_dialogue = next_dia;
+	next_dialogue_name = next_dia_name;
+	next_pos = next_p;
+	next_max_dist = next_max_d;
+	next_signal = next_sign;
+	
+
+func _process(delta):
+	if(next_dialogue):
+		var dist = player.get_global_position() - next_pos;
+		if(dist.length() <= next_max_dist):
+			DialogueManager.show_dialogue_balloon(next_dialogue, next_dialogue_name);
+			next_signal.emit();
+			next_dialogue = null;
+		
